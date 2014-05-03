1a1754156bb038d2fb8264df3a8d475db65c4846
slavomirvojacek <slavomirvojacek@gmail.com>
2014-05-03 19:51:06 +0200

    File clearance

--- www/media/css/screen.php
+++ www/media/css/screen.php
@@ -4,11 +4,11 @@ header("Content-Type: text/css", true, 200);
 header("Cache-Control: no-cache, must-revalidate", true);
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT", true);
 
-require_once "_compiler/lessc.php";
+require_once "_compiler/scss.php";
 
-$parser = new Less_Parser();
-$parser->parseFile("screen.less");
+$scss = new scssc();
+$scss->setFormatter("scss_formatter_compressed");
+$compiled = $scss->compile(file_get_contents("screen.scss"));
 
-file_put_contents(__DIR__ . "/screen.css", $parser->getCss(), LOCK_EX);
-
-echo $parser->getCss();
+file_put_contents(__DIR__ . "/screen.css", $compiled, LOCK_EX);
+echo $compiled;
