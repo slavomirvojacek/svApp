b594d5ae395819aa31882515992aebc169802e1c
Slavomir Vojacek <slavomir@adbrain.com>
2014-05-19 08:59:24 +0100

    Fixed library randomisation

--- lib/__check.php
+++ lib/__check.php
@@ -7,7 +7,7 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
 
 $usage = array(
 		"usage" => getrusage(),
-		"randKey" => bin2hex(openssl_random_pseudo_bytes(11))
+		"randKey" => (string) bin2hex(openssl_random_pseudo_bytes(11))
 );
 
 $initDir = glob(__DIR__ . "/*", GLOB_ONLYDIR)[0];
@@ -17,8 +17,15 @@ $initDir = glob(__DIR__ . "/*", GLOB_ONLYDIR)[0];
  * if library randomisation is enabled
  */
 if (LIB_RANDOMISATION) {
-	$newDir = APP_DIR . "/lib/" . $usage["randKey"];
-	rename($initDir, $newDir);
+	$len = mb_strlen($initDir);
+
+	if ($len == 64) {
+		$newDir = $initDir;
+	}
+	else {
+		$newDir = APP_DIR . "/lib/" . $usage["randKey"];
+		rename($initDir, $newDir);
+	}
 }
 else {
 	$newDir = $initDir;
