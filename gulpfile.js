3ada5aa45b9f40edcb8f18d820e2fb02cbc95c6d
Slavomir Vojacek <slavomir@adbrain.com>
2014-07-24 15:37:21 +0100

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp =
 	require("gulp"),
 	sass = require("gulp-ruby-sass"),
 	autoprefixer = require("gulp-autoprefixer"),
-	uglify = require("gulp-uglify"),
+	uglify = require("gulp-uglify");
 
 
 /*
